ce038090e61aca57c422843de5e5a1d70b1da334
Maxime Moreillon <m.moreillon@gmail.com>
2020-02-12 23:50:39 +0000

    switched to JWT entirely

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors')
 const history = require('connect-history-api-fallback');
 const bcrypt = require('bcrypt');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // custom modules
 const secrets = require('./secrets');
@@ -16,15 +16,9 @@ const secrets = require('./secrets');
 const app_port = 8088;
 const saltRounds = 10;
 
-const login_status = (req) => {
-  return typeof(req.session.username) !== 'undefined'
-}
 
 const send_login_status = (req, res) => {
-  res.send({
-    logged_in: login_status(req),
-    username:req.session.username,
-  });
+  res.send("Not implemented yet");
 }
 
 // Express configuration
@@ -34,99 +28,45 @@ app.use(history({
   // Ignore GET routes
   rewrites: [
     { from: '/status', to: '/status'},
-    { from: '/logout', to: '/logout'},
   ]
 }));
 
 // Serve the front end (single page application created in Vue)
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.use(cors({
-  //origin: misc.cors_origins,
-  origin: (origin, callback) => {
-    callback(null, true)
-  },
-  credentials: true,
-}));
-
-app.use(cookieSession({
-  name: 'session',
-  secret: secrets.session_secret,
-  maxAge: 253402300000000,
-  sameSite: false,
-  domain: secrets.cookies_domain
-}));
-
-app.post('/status', send_login_status);
-app.get('/status', send_login_status);
+app.use(cors());
 
 app.post('/login', (req, res) => {
 
   // Check if all necessary login information is provided
   if( !('username' in req.body && 'password' in req.body) ){
-    return res.status(403).send({
-      logged_in: login_status(req),
-      error: 'Missing username or password',
-    });
+    // 400: Bad request
+    return res.status(400).send('Missing username or password');
   }
 
   // Check if username matches
+  // (Only one user at the moment)
   if(req.body.username !== secrets.username){
-    return res.status(403).send({
-      logged_in: login_status(req),
-      error: "Invalid username",
-    })
+    return res.status(400).send('Invalid username')
   }
 
   // Now check if the password is correct
   bcrypt.compare(req.body.password, secrets.password_hashed, (err, result) => {
-    if(err) return res.status(500).send({
-      logged_in: login_status(req),
-      error: "Error checking password",
-    })
+    if(err) return res.status(500).send('Error while verifying password')
     if(result) {
-
-      // Setting session variable
-      req.session.username = req.body.username;
 
       // Generate JWT
       jwt.sign({ username: req.body.username }, secrets.jwt_secret, (err, token) => {
-        if(err) return res.status(500).send({
-          logged_in: login_status(req),
-          error: "Error generating token",
-        })
+        if(err) return res.status(500).send('Error generating token')
 
-        // Send success acknowledgement
-        res.send({
-          logged_in: login_status(req),
-          username: req.session.username,
-          jwt: token
-        });
+        // Respond with JWT
+        res.send({jwt: token});
 
       });
     }
-    else {
-      res.status(403).send({
-        logged_in: login_status(req),
-        error: "Incorrect password",
-        password_hashed: secrets.password_hashed,
-        password: req.body.password,
-      });
-    }
+    else res.status(403).send('Incorrect password');
   });
 })
-
-app.post('/logout', (req, res) => {
-  delete req.session.username;
-  res.send({
-    logged_in: typeof(req.session.username) !== 'undefined',
-  });
-});
-
-app.get('/logout', (req, res) => {
-  delete req.session.username;
-  res.redirect(req.get('Referrer'));
-});
 
 
 // Start server
