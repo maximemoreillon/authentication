8f3ce8885d85bbafb1ca0dcfbedb07e01ff17431
Maxime Moreillon <m.moreillon@gmail.com>
2020-03-06 02:16:08 +0000

    now using Neo4J as user database

--- server.js
+++ server.js
@@ -9,17 +9,19 @@ const cors = require('cors')
 const history = require('connect-history-api-fallback');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const neo4j = require('neo4j-driver');
 
 // custom modules
 const secrets = require('./secrets');
 
+const driver = neo4j.driver(
+  secrets.neo4j.url,
+  neo4j.auth.basic(secrets.neo4j.username, secrets.neo4j.password)
+)
+
+// Config
 const app_port = 7088;
 const saltRounds = 10;
-
-
-const send_login_status = (req, res) => {
-  res.send("Not implemented yet");
-}
 
 // Express configuration
 const app = express();
@@ -44,28 +46,44 @@ app.post('/login', (req, res) => {
     return res.status(400).send('Missing username or password');
   }
 
-  // Check if username matches
-  // (Only one user at the moment)
-  if(req.body.username !== secrets.username){
-    return res.status(400).send('Invalid username')
-  }
+  var session = driver.session()
+  session
+  .run(`
+    MATCH (user:User {username: {username}})
+    RETURN user
+    `, {
+      username: req.body.username,
+    })
+  .then(result => {
+    session.close()
 
-  // Now check if the password is correct
-  bcrypt.compare(req.body.password, secrets.password_hashed, (err, result) => {
-    if(err) return res.status(500).send('Error while verifying password')
-    if(result) {
+    if(result.records.length === 0) return res.status(400).send('Invalid username')
 
-      // Generate JWT
-      jwt.sign({ username: req.body.username }, secrets.jwt_secret, (err, token) => {
-        if(err) return res.status(500).send('Error generating token')
+    let user_from_DB = result.records[0]._fields[result.records[0]._fieldLookup['user']]
 
-        // Respond with JWT
-        res.send({jwt: token});
+    // Now check if the password is correct
+    bcrypt.compare(req.body.password, user_from_DB.properties.password_hashed, (err, result) => {
+      if(err) return res.status(500).send('Error while verifying password')
+      if(result) {
 
-      });
-    }
-    else res.status(403).send('Incorrect password');
-  });
+        // Generate JWT
+        jwt.sign({ username: req.body.username }, secrets.jwt_secret, (err, token) => {
+          if(err) return res.status(500).send('Error generating token')
+
+          // Respond with JWT
+          res.send({jwt: token});
+
+        });
+      }
+      else res.status(403).send('Incorrect password');
+    });
+
+  })
+  .catch(error => {
+    res.status(500).send(`Error getting articles: ${error}`)
+  })
+
+
 })
 
 
