9f1ebae4a46355349b7185f2aaa654695b8d12d4
Maxime Moreillon <m.moreillon@gmail.com>
2020-04-19 14:59:39 +0000

    redeployment

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ var app_port = 80
 if(process.env.APP_PORT) app_port=process.env.APP_PORT
 const saltRounds = 10;
 
-
 function verify_jwt_and_respond_with_user(token, res){
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if(err) return res.status(403).send('Invalid JWT')
