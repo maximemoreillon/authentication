ec0a67a44a8083a4e62fae93545a3c153081300f
Maxime Moreillon <m.moreillon@gmail.com>
2021-11-28 14:31:57 +0000

    async await

--- controllers/v2/authentication.js
+++ controllers/v2/authentication.js
@@ -20,137 +20,103 @@ const register_last_login = (user_id) => {
   const parameters = { user_id }
   const session = driver.session()
 
+  return session.run( query, parameters )
+
+
+}
+
+const find_user_in_db = (identifier) => new Promise ( (resolve, reject) => {
+
+  const query = `
+    MATCH (user:User)
+
+    // Allow user to identify using either userrname or email address
+    WHERE user.username=$identifier
+      OR user.email_address=$identifier
+      OR id(user) = toInteger($identifier)
+
+    // Return user if found
+    RETURN user
+    `
+
+  const parameters = {identifier}
+
+  const session = driver.session()
   session
-    .run( query, parameters )
-    .then(() => { console.log(`[Auth] Successfully registered last login for user ${user_id}`) })
-    .catch((error) => { console.log(`[Auth] Error setting last login: ${error}`) })
-    .finally( () => { session.close() })
+  .run(query, parameters)
+  .then( ({records}) => {
 
-}
+    if(records.length < 1) return reject({code: 400, message: `User ${identifier} not found`})
+    if(records.length > 1) return reject({code: 500, message: `Multiple users found`})
 
-const find_user_in_db = (identifier) => {
+    const user = records[0].get('user')
 
-  return new Promise ( (resolve, reject) => {
+    if(user.properties.locked) return reject({code: 500, message: `User account ${user.identity} is locked`})
 
-    const query = `
-      MATCH (user:User)
+    resolve(user)
 
-      // Allow user to identify using either userrname or email address
-      WHERE user.username=$identifier
-        OR user.email_address=$identifier
-        OR id(user) = toInteger($identifier)
-
-      // Return user if found
-      RETURN user
-      `
-
-    const parameters = {identifier}
-
-    const session = driver.session()
-    session
-    .run(query, parameters)
-    .then( ({records}) => {
-
-      if(records.length < 1) return reject({code: 400, message: `User ${identifier} not found`})
-      if(records.length > 1) return reject({code: 500, message: `Multiple users found`})
-
-      const user = records[0].get('user')
-
-      if(user.properties.locked) return reject({code: 500, message: `User account ${user.identity} is locked`})
-
-      resolve(user)
-
-      console.log(`[Neo4J] User ${user.identity} found in the DB`)
-
-    })
-    .catch(error => { reject({code: 500, message:error}) })
-    .finally( () => session.close())
+    console.log(`[Neo4J] User ${user.identity} found in the DB`)
 
   })
-}
+  .catch(error => { reject({code: 500, message:error}) })
+  .finally( () => session.close())
 
-const check_password = (password_plain, user) => {
-  return new Promise ( (resolve, reject) => {
+})
 
-    // Retrieve hashed password from user properties
-    const password_hashed = user.properties.password_hashed
 
-    // check if the user has a password
-    if(!password_hashed) return reject({code: 500, message: `User ${user.identity} does not have a password`})
+const check_password = (password_plain, password_hashed) => bcrypt.compare(password_plain, password_hashed)
 
-    bcrypt.compare(password_plain, password_hashed, (error, password_correct) => {
+const generate_token = (user) => new Promise( (resolve, reject) => {
 
-      // Handle check errors
-      if(error) return reject({code: 500, message: error})
+  // Check if the secret is set
+  if(!jwt_secret) return reject({code: 500, message: `Token secret not set`})
 
-      // Handle incoree
-      if(!password_correct) return reject({code: 403, message: `Incorrect password`})
+  const token_content = { user_id: user.identity }
 
-      resolve(user)
+  jwt.sign(token_content, jwt_secret, (error, token) => {
 
-      console.log(`[Auth] Password correct for user ${user.identity}`)
+    // handle signing errors
+    if(error) return reject({code: 500, message: error})
 
-    })
-
-  })
-}
-
-const generate_token = (user) => {
-  return new Promise( (resolve, reject) => {
-
-    // Check if the secret is set
-    if(!jwt_secret) return reject({code: 500, message: `Token secret not set`})
-
-    const token_content = { user_id: user.identity }
-
-    jwt.sign(token_content, jwt_secret, (error, token) => {
-
-      // handle signing errors
-      if(error) return reject({code: 500, message: error})
-
-      // Resolve with token
-      resolve(token)
-
-      console.log(`[Auth] Token generated for user ${user.identity}`)
-
-    })
-  })
-}
-
-const verify_token = (token) => {
-  return new Promise ( (resolve, reject) => {
-
-    // Check if the secret is set
-    if(!jwt_secret) return reject({code: 500, message: `Token secret not set`})
-
-    jwt.verify(token, jwt_secret, (error, decoded_token) => {
-
-      if(error) return reject({code: 403, message: `Invalid JWT`})
-
-      resolve(decoded_token)
-
-      console.log(`[Auth] Token decoded successfully`)
-
-    })
-  })
-}
-
-const retrieve_token_from_body_or_query = (req) => {
-  return new Promise ( (resolve, reject) => {
-
-    const token = req.body.token
-      || req.body.jwt
-      || req.query.jwt
-      || req.query.token
-
-    if(!token) return reject({code: 400, message: `Missing token`})
-
+    // Resolve with token
     resolve(token)
 
-    console.log(`[Auth] Token retrieved from body or query`)
+    console.log(`[Auth] Token generated for user ${user.identity}`)
 
   })
-}
+})
+
+const verify_token = (token) => new Promise ( (resolve, reject) => {
+
+  // Check if the secret is set
+  if(!jwt_secret) return reject({code: 500, message: `Token secret not set`})
+
+  jwt.verify(token, jwt_secret, (error, decoded_token) => {
+
+    if(error) return reject({code: 403, message: `Invalid JWT`})
+
+    resolve(decoded_token)
+
+    console.log(`[Auth] Token decoded successfully`)
+
+  })
+})
+
+const retrieve_token_from_body_or_query = (req) => new Promise ( (resolve, reject) => {
+
+  const token = req.body.token
+    || req.body.jwt
+    || req.query.jwt
+    || req.query.token
+
+  if(!token) return reject({code: 400, message: `Missing token`})
+
+  resolve(token)
+
+  console.log(`[Auth] Token retrieved from body or query`)
+
+})
+
 
 const retrieve_token_from_headers = (req) => {
   return new Promise ( (resolve, reject) => {
@@ -168,95 +134,105 @@ const retrieve_token_from_headers = (req) => {
   })
 }
 
+const retrieve_jwt = (req, res) => new Promise( (resolve, reject) => {
+
+  const jwt = req.headers.authorization?.split(" ")[1]
+    || (new Cookies(req, res)).get('jwt')
+    || req.query.jwt
+    || req.query.token
+
+  if(!jwt) return reject(`JWT not provided`)
+
+  resolve(jwt)
+})
 
 
-exports.login = (req, res) => {
 
-  // Input sanitation
-  const identifier = req.body.username
-    || req.body.email_address
-    || req.body.email
-    || req.body.identifier
+exports.login = async (req, res) => {
 
-  const password = req.body.password
+  try {
+    // Input sanitation
+    const identifier = req.body.username
+      || req.body.email_address
+      || req.body.email
+      || req.body.identifier
 
-  if(!identifier) return res.status(400).send(`Missing username or e-mail address`)
-  if(!password) return res.status(400).send(`Missing password`)
+    const password = req.body.password
 
-  console.log(`[Auth] Login attempt from user identified as ${identifier}`)
+    if(!identifier) return res.status(400).send(`Missing username or e-mail address`)
+    if(!password) return res.status(400).send(`Missing password`)
 
-  find_user_in_db(identifier)
-  .then( user =>  check_password(password, user) )
-  .then( user => {
-    // Save the last login time of the user
-    register_last_login(user.identity)
+    console.log(`[Auth] Login attempt from user identified as ${identifier}`)
 
-    return generate_token(user)
-  })
-  .then( token => { res.send({ jwt: token }) })
-  .catch(error => {
+    const user = await find_user_in_db(identifier)
+    const {password_hashed, identity} = user.properties
+    if(!password_hashed) throw 'User does not have a password'
+    await check_password(password, password_hashed)
+    await register_last_login(identity)
+    const token = generate_token(user)
+
+    res.send({ jwt: token })
+  } catch (error) {
     console.log(error.message || error)
     res.status(error.code || 500).send(error.message || error)
-  })
+  }
+
 }
 
-exports.whoami = (req, res) => {
+exports.whoami = async (req, res) => {
   // Retrieves user information based on JWT present in auth header
 
-  retrieve_token_from_headers(req)
-  .then( token =>  verify_token(token) )
-  .then( decoded_token => {
-
-    const user_id = decoded_token.user_id
+  try {
+    const token = await retrieve_jwt(req, res)
+    const {user_id} = verify_token(token)
     if(!user_id) throw {code: 400, message: `No user ID in token`}
+    const user = await find_user_in_db(user_id)
 
-    return find_user_in_db(user_id)
-
-  })
-  .then( user => {
+    // Hide password_hashed from response
     delete user.properties.password_hashed
+
     res.send(user)
     console.log(`[Auth] user ${user.identity} retrieved using token`)
-  })
-  .catch(error => {
-    console.log(`[Auth] ${error.message || error}`)
-    res.status(error.code || 500).send(error.message || error)
-  })
-}
 
-exports.decode_token = (req, res) => {
-
-  // Not useful not widely used
-
-  retrieve_token_from_body_or_query(req)
-  .then( token => verify_token(token) )
-  .then( decoded_token => { res.send(decoded_token) })
-  .catch(error => {
+  } catch (error) {
     console.log(error.message || error)
     res.status(error.code || 500).send(error.message || error)
-  })
+  }
 
 }
 
-exports.get_user_from_jwt = (req, res) => {
+exports.decode_token = async (req, res) => {
 
-  retrieve_token_from_body_or_query(req)
-  .then( token => verify_token(token) )
-  .then( decoded_token => {
+  // Not useful nor widely used
 
-    const user_id = decoded_token.user_id
+  try {
+    const token = await retrieve_token_from_body_or_query(req)
+    const decoded_token = await verify_token(token)
+    res.send(decoded_token)
+    console.log(`[Auth] Token with content ${decoded_token} decoded`)
+
+  } catch (error) {
+    console.log(error.message || error)
+    res.status(error.code || 500).send(error.message || error)
+  }
+
+}
+
+exports.get_user_from_jwt = async (req, res) => {
+
+  try {
+    const token = await retrieve_token_from_body_or_query(req)
+    const {user_id} = await verify_token(token)
     if(!user_id) throw {code: 400, message: `No user ID in token`}
-
-    return find_user_in_db(user_id)
-
-  })
-  .then( user => {
+    const user = await find_user_in_db(user_id)
+    // Hide password_hashed from response
     delete user.properties.password_hashed
     res.send(user)
     console.log(`[Auth] user ${user.identity} retrieved using token`)
-  })
-  .catch(error => {
-    console.log(`[Auth] ${error.message || error}`)
+
+  } catch (error) {
+    console.log(error.message || error)
     res.status(error.code || 500).send(error.message || error)
-  })
+  }
+
 }
