928caf9df5be57186ee91a54a38dfd77206f7960
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-30 08:38:25 +0900

    hunting down referrers

--- controllers/v1/authentication.js
+++ controllers/v1/authentication.js
@@ -1,7 +1,9 @@
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-const {drivers: {v1: driver}} = require('../../db.js')
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const dotenv = require("dotenv")
+const {
+  drivers: { v1: driver },
+} = require("../../db.js")
 const {
   error_handling,
   user_query,
@@ -14,107 +16,109 @@ const {
   find_user_in_db,
   find_user_by_id,
   register_last_login,
-} = require('../../utils.js')
+} = require("../../utils.js")
 
 dotenv.config()
 
-
-
-
-
-
-
-
-
-
 exports.login = async (req, res) => {
+  const referrer = req.get("Referrer")
+  console.log(`[/v1/login] referrer: ${referrer}`)
 
   try {
-    const identifier = req.body.username
-      || req.body.email_address
-      || req.body.email
-      || req.body.identifier
+    const identifier =
+      req.body.username ||
+      req.body.email_address ||
+      req.body.email ||
+      req.body.identifier
 
-    const {password} = req.body
+    const { password } = req.body
 
-    if(!identifier) return res.status(400).send(`Missing username or e-mail address`)
-    if(!password) return res.status(400).send(`Missing password`)
+    if (!identifier)
+      return res.status(400).send(`Missing username or e-mail address`)
+    if (!password) return res.status(400).send(`Missing password`)
 
     console.log(`[Auth V1] Login attempt from user identified as ${identifier}`)
 
-    const user = await find_user_in_db({driver,identifier})
+    const user = await find_user_in_db({ driver, identifier })
 
-    if(user.properties.locked) throw {code: 403, message: `User account ${identifier} is locked`}
+    if (user.properties.locked)
+      throw { code: 403, message: `User account ${identifier} is locked` }
 
-    const {password_hashed, _id} = user.properties
-    if(!password_hashed) throw {code: 500, message: 'User does not have a password'}
+    const { password_hashed, _id } = user.properties
+    if (!password_hashed)
+      throw { code: 500, message: "User does not have a password" }
 
     await check_password(password, password_hashed)
 
-    await register_last_login({driver, user_id:_id })
+    await register_last_login({ driver, user_id: _id })
 
     const token = await generate_token(user)
     res.send({ jwt: token })
-
-  }
-
-  catch (error) {
+  } catch (error) {
     error_handling(error, res)
   }
-
 }
 
 exports.whoami = (req, res) => {
   // Retrieves user information based on JWT present in auth header
 
+  const referrer = req.get("Referrer")
+  console.log(`[/v1/whoami] referrer: ${referrer}`)
+
   retrieve_jwt(req)
-  .then( token => {return verify_token(token)})
-  .then( decoded_token => {
+    .then((token) => {
+      return verify_token(token)
+    })
+    .then((decoded_token) => {
+      // low should not be a thing!
+      const user_id = decoded_token.user_id.low || decoded_token.user_id
+      if (!user_id) throw { code: 400, message: `No user ID in token` }
 
-    // low should not be a thing!
-    const user_id = decoded_token.user_id.low || decoded_token.user_id
-    if(!user_id) throw {code: 400, message: `No user ID in token`}
-
-    return find_user_by_id({driver,user_id})
-
-  })
-  .then( user => {
-    delete user.properties.password_hashed
-    console.log(`[Auth v1] user ${user.properties._id} retrieved using token`)
-    res.send(user)
-  })
-  .catch(error => {
-    error_handling(error, res)
-  })
+      return find_user_by_id({ driver, user_id })
+    })
+    .then((user) => {
+      delete user.properties.password_hashed
+      console.log(`[Auth v1] user ${user.properties._id} retrieved using token`)
+      res.send(user)
+    })
+    .catch((error) => {
+      error_handling(error, res)
+    })
 }
 
 exports.decode_token = (req, res) => {
   retrieve_token_from_body_or_query(req)
-  .then( token => {return verify_token(token)})
-  .then(decoded_token => { res.send(decoded_token) })
-  .catch(error => {
-    error_handling(error, res)
-  })
+    .then((token) => {
+      return verify_token(token)
+    })
+    .then((decoded_token) => {
+      res.send(decoded_token)
+    })
+    .catch((error) => {
+      error_handling(error, res)
+    })
 }
 
 exports.get_user_from_jwt = (req, res) => {
+  const referrer = req.get("Referrer")
+  console.log(`[/v1/user_from_jwt] referrer: ${referrer}`)
 
   // wrongly used in router
   retrieve_token_from_body_or_query(req)
-  .then( token => {return verify_token(token)})
-  .then( decoded_token => {
+    .then((token) => {
+      return verify_token(token)
+    })
+    .then((decoded_token) => {
+      const user_id = decoded_token.user_id.low || decoded_token.user_id
+      if (!user_id) throw { code: 400, message: `No user ID in token` }
 
-    const user_id = decoded_token.user_id.low || decoded_token.user_id
-    if(!user_id) throw {code: 400, message: `No user ID in token`}
-
-    return find_user_by_id({driver,user_id})
-
-  })
-  .then( user => {
-    delete user.properties.password_hashed
-    res.send(user)
-  })
-  .catch(error => {
-    error_handling(error, res)
-  })
+      return find_user_by_id({ driver, user_id })
+    })
+    .then((user) => {
+      delete user.properties.password_hashed
+      res.send(user)
+    })
+    .catch((error) => {
+      error_handling(error, res)
+    })
 }
--- controllers/v2/authentication.js
+++ controllers/v2/authentication.js
@@ -1,4 +1,6 @@
-const {drivers: {v2: driver}} = require('../../db.js')
+const {
+  drivers: { v2: driver },
+} = require("../../db.js")
 const {
   error_handling,
   user_query,
@@ -10,67 +12,70 @@ const {
   retrieve_token_from_body_or_query,
   find_user_in_db,
   find_user_by_id,
-  register_last_login
-} = require('../../utils.js')
-
+  register_last_login,
+} = require("../../utils.js")
 
 exports.login = async (req, res) => {
+  const referrer = req.get("Referrer")
+  console.log(`[/v3/login] referrer: ${referrer}`)
 
   try {
-    const identifier = req.body.username
-      || req.body.email_address
-      || req.body.email
-      || req.body.identifier
+    const identifier =
+      req.body.username ||
+      req.body.email_address ||
+      req.body.email ||
+      req.body.identifier
 
-    const {password} = req.body
+    const { password } = req.body
 
-    if(!identifier) return res.status(400).send(`Missing username or e-mail address`)
-    if(!password) return res.status(400).send(`Missing password`)
+    if (!identifier)
+      return res.status(400).send(`Missing username or e-mail address`)
+    if (!password) return res.status(400).send(`Missing password`)
 
     console.log(`[Auth v2] Login attempt from user identified as ${identifier}`)
 
-    const user = await find_user_in_db({driver,identifier})
+    const user = await find_user_in_db({ driver, identifier })
 
-    if(user.properties.locked) throw {code: 403, message: `User account ${identifier} is locked`}
+    if (user.properties.locked)
+      throw { code: 403, message: `User account ${identifier} is locked` }
 
-    const {password_hashed, _id} = user.properties
-    if(!password_hashed) throw {code: 500, message: 'User does not have a password'}
+    const { password_hashed, _id } = user.properties
+    if (!password_hashed)
+      throw { code: 500, message: "User does not have a password" }
     await check_password(password, password_hashed)
 
-    await register_last_login({driver, user_id:_id })
+    await register_last_login({ driver, user_id: _id })
 
     const token = await generate_token(user)
     res.send({ jwt: token })
-  }
-  catch (error) {
+  } catch (error) {
     error_handling(error, res)
   }
-
 }
 
 exports.whoami = async (req, res) => {
   // Retrieves user information based on JWT present in auth header
 
+  const referrer = req.get("Referrer")
+  console.log(`[/v2/whoami] referrer: ${referrer}`)
+
   try {
     const token = await retrieve_jwt(req, res)
-    const {user_id} = await verify_token(token)
-    if(!user_id) throw {code: 400, message: `No user ID in token`}
-    const user = await find_user_by_id({driver,user_id})
+    const { user_id } = await verify_token(token)
+    if (!user_id) throw { code: 400, message: `No user ID in token` }
+    const user = await find_user_by_id({ driver, user_id })
 
     // Hide password_hashed from response
     delete user.properties.password_hashed
 
     res.send(user)
     console.log(`[Auth v2] user ${user_id} retrieved using token`)
-
   } catch (error) {
     error_handling(error, res)
   }
-
 }
 
 exports.decode_token = async (req, res) => {
-
   // Not useful nor widely used
 
   try {
@@ -78,27 +83,25 @@ exports.decode_token = async (req, res) => {
     const decoded_token = await verify_token(token)
     res.send(decoded_token)
     console.log(`[Auth v2] Token with content ${decoded_token} decoded`)
-
   } catch (error) {
     error_handling(error, res)
   }
-
 }
 
 exports.get_user_from_jwt = async (req, res) => {
+  const referrer = req.get("Referrer")
+  console.log(`[/v2/user_from_jwt] referrer: ${referrer}`)
 
   try {
     const token = await retrieve_token_from_body_or_query(req)
-    const {user_id} = await verify_token(token)
-    if(!user_id) throw {code: 400, message: `No user ID in token`}
-    const user = await find_user_by_id({driver,user_id})
+    const { user_id } = await verify_token(token)
+    if (!user_id) throw { code: 400, message: `No user ID in token` }
+    const user = await find_user_by_id({ driver, user_id })
     // Hide password_hashed from response
     delete user.properties.password_hashed
     res.send(user)
     console.log(`[Auth v2] user ${user_id} retrieved using token`)
-
   } catch (error) {
     error_handling(error, res)
   }
-
 }
